b5cf5c15305be781757779a46981d2390108d214
simhanger <simhanger@gmail.com>
2022-07-15 01:01:12 +0900

    JSch example  Channel 복제 테스트

---
+++ src/main/java/com/test/jsch/test/JSchShell.java
@@ -0,0 +1,33 @@
+/*
+ * Created by JFormDesigner on Thu Jul 14 23:40:18 KST 2022
+ */
+
+package com.test.jsch.test;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author unknown
+ */
+public class JSchShell extends JFrame {
+    public JSchShell() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
